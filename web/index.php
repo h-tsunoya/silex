32bb98a97ff5d7fa79d6db9f92bba27c43a724a6
tsunoya <h-tsunoya@marryplan.com>
2016-09-05 07:17:00 +0900

    first commit

---
+++ web/index.php
@@ -0,0 +1,48 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+ 
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__ . '/../views',
+));
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver' => 'pdo_mysql',
+        'dbname' => 'silex',
+        'host' => 'localhost',
+        'user' => '',
+        'password' => ''
+    )
+));
+ 
+$app->get('/hello/{name}', function ($name) use ($app) {
+  return 'Hello '.$app->escape($name);
+});
+
+$app->get('/member/register', function() use($app) {
+    return $app['twig']->render('member/register.html.twig');
+}); 
+
+$app->post('/member/register', function() use($app){
+    $request = $app['request'];
+    $member = $request->get('member');
+ 
+    $stmt = $app['db']->prepare("
+       INSERT INTO member SET
+         email = :email
+        ,password = :password
+    ");
+    $stmt->bindParam(':email', $member['email']);
+    $password = md5($member['password']);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+ 
+    return $app['twig']->render('member/finish.html.twig', array(
+        'member' => $member
+    ));
+});
+ 
+$app->run();
